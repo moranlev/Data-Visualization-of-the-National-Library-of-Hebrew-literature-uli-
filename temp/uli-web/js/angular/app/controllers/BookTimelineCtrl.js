4df338b275bd42224c78170818d754ca5e26045a
moranlev <ms1lev@gmail.com>
2016-06-07 13:20:13 +0200

    DATA POPUP

--- temp/uli-web/js/angular/app/controllers/BookTimelineCtrl.js
+++ temp/uli-web/js/angular/app/controllers/BookTimelineCtrl.js
@@ -3,6 +3,8 @@ myApp.controller('BookTimeLineCtrl', function (dataService, $scope, $uibModal) {
     $scope.bookDic = {};
     $scope.maxBooksPerYear = 0;
 
+    $scope.bookdatapars = {};
+
     $scope.init = function () {
         console.log('inside the loop ' + dataService.length);
         for (var i = 0; i < dataService.length; i++) {
@@ -11,6 +13,7 @@ myApp.controller('BookTimeLineCtrl', function (dataService, $scope, $uibModal) {
                 $scope.bookDic[bookIterator.year] = [];
             }
             $scope.bookDic[bookIterator.year].push(bookIterator);
+
         }
         for (var key in $scope.bookDic) {
             if ($scope.maxBooksPerYear < $scope.bookDic[key].length) {
@@ -32,6 +35,25 @@ myApp.controller('BookTimeLineCtrl', function (dataService, $scope, $uibModal) {
         console.log(perc);
         return { width: perc + '%' };
     }
+
+
+    $scope.data = function () {
+       
+        for (var i = 0; i < dataService.length; i++) {
+            var bookdata = dataService[i];
+           
+            if (!$scope.bookdatapars[bookdata.fulltitle]) {
+                $scope.bookdatapars[bookdata.fulltitle] = [];
+                console.log('MORAN' + $scope.bookdatapars);
+                console.log('MORAN' + $scope.bookdata);
+            }
+            $scope.bookdatapars[bookdata.fulltitle].push(bookdata);
+        }
+    
+       
+       
+    }
+    $scope.data();
 
 
 });
